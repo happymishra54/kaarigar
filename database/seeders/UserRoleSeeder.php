7381b3ec0d4932455c3a10722d426b09ca4dae8b
happymishra54 <140776425+happymishra54@users.noreply.github.com>
2026-06-23 09:18:17 +0530

    make the seeders stop inserting data

--- database/seeders/UserRoleSeeder.php
+++ database/seeders/UserRoleSeeder.php
@@ -8,36 +8,36 @@ use Illuminate\Support\Facades\Hash;
 
 class UserRoleSeeder extends Seeder
 {
-    public function run(): void
-    {
-        // 1. Create the Admin User
-        User::updateOrCreate(
-            ['email' => 'admin@kaarigar.com'], // Checks if this email exists
-            [
-                'name' => 'Admin',
-                'password' => Hash::make('admin123456'),
-                'role' => 'admin', // Sets the role as admin
-            ]
-        );
+    // public function run(): void
+    // {
+    //     // 1. Create the Admin User
+    //     User::updateOrCreate(
+    //         ['email' => 'admin@kaarigar.com'], // Checks if this email exists
+    //         [
+    //             'name' => 'Admin',
+    //             'password' => Hash::make('admin123456'),
+    //             'role' => 'admin', // Sets the role as admin
+    //         ]
+    //     );
 
-        // 2. Create a Worker User
-        User::updateOrCreate(
-            ['email' => 'worker@kaarigar.com'],
-            [
-                'name' => 'Worker',
-                'password' => Hash::make('worker123456'),
-                'role' => 'worker', // Sets the role as worker
-            ]
-        );
+    //     // 2. Create a Worker User
+    //     User::updateOrCreate(
+    //         ['email' => 'worker@kaarigar.com'],
+    //         [
+    //             'name' => 'Worker',
+    //             'password' => Hash::make('worker123456'),
+    //             'role' => 'worker', // Sets the role as worker
+    //         ]
+    //     );
 
-        // 3. Create a Customer User
-        User::updateOrCreate(
-            ['email' => 'customer@kaarigar.com'],
-            [
-                'name' => 'Customer',
-                'password' => Hash::make('customer123456'),
-                'role' => 'customer', // Sets the role as customer
-            ]
-        );
-    }
+    //     // 3. Create a Customer User
+    //     User::updateOrCreate(
+    //         ['email' => 'customer@kaarigar.com'],
+    //         [
+    //             'name' => 'Customer',
+    //             'password' => Hash::make('customer123456'),
+    //             'role' => 'customer', // Sets the role as customer
+    //         ]
+    //     );
+    // }
 }
