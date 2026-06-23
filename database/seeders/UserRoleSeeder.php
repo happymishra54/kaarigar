914cc71004563e4129128aa8ead858731154209f
happymishra54 <140776425+happymishra54@users.noreply.github.com>
2026-06-23 09:23:58 +0530

    make the seeders stop inserting data

--- database/seeders/UserRoleSeeder.php
+++ database/seeders/UserRoleSeeder.php
@@ -8,17 +8,18 @@ use Illuminate\Support\Facades\Hash;
 
 class UserRoleSeeder extends Seeder
 {
-    // public function run(): void
-    // {
-    //     // 1. Create the Admin User
-    //     User::updateOrCreate(
-    //         ['email' => 'admin@kaarigar.com'], // Checks if this email exists
-    //         [
-    //             'name' => 'Admin',
-    //             'password' => Hash::make('admin123456'),
-    //             'role' => 'admin', // Sets the role as admin
-    //         ]
-    //     );
+    public function run(): void
+    {
+        // 1. Create the Admin User
+        User::updateOrCreate(
+            ['email' => 'admin@kaarigar.com'], // Checks if this email exists
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('Admin.@8825'), // Hashes the password
+                'role' => 'admin', // Sets the role as admin
+            ]
+        );
+    }
 
     //     // 2. Create a Worker User
     //     User::updateOrCreate(
